77b8e56065f8159760f2d9c91690b2297d7262b2
0xSYN.dev <pheonixgamer12345@gmail.com>
2026-01-26 22:46:38 +0530

    feat: add send verify email logic

--- src/controllers/auth/auth.controller.ts
+++ src/controllers/auth/auth.controller.ts
@@ -2,6 +2,15 @@ import { registerSchema } from "./auth.schema";
 import { Request, Response } from "express";
 import { User } from "../../models/user.model";
 import { hashPassword } from "../../lib/hash";
+import jwt from "jsonwebtoken";
+import { sendEmail } from "../../lib/email";
+
+function getAppUrl() {
+    if (process.env.NODE_ENV === "development") {
+        return `http://localhost:${process.env.PORT}`;
+    }
+    return process.env.APP_URL! || `http://localhost:${process.env.PORT}`;
+}
 
 export async function registerHandler(req: Request, res: Response) {
     try {
@@ -28,7 +37,24 @@ export async function registerHandler(req: Request, res: Response) {
         });
 
         // Email verification logic
-        
+        const verifyToken = jwt.sign({ id: newUser._id }, process.env.JWT_ACCESS_SECRET!, { expiresIn: "1d" });
+
+        const verifyUrl = `${getAppUrl()}/auth/verify-email?token=${verifyToken}`;
+
+        await sendEmail(
+            email,
+            "Verify your email",
+            `Please click on the link below to verify your email: <a href="${verifyUrl}">Verify Email</a>`
+        );
+
+        return res.status(201).json({
+            message: "User registered successfully", user: {
+                name,
+                email,
+                role: newUser.role,
+                isEmailVerified: newUser.isEmailVerified,
+            }
+        });
 
     } catch (error) {
         return res.status(500).json({ message: "Internal server error", error });
